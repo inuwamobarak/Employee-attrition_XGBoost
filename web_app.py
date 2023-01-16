311d3b564d3503a26ca4395ec7c5bc2f5a7be7f9
Inuwa Mobarak Abraham <inuwamobarak@gmail.com>
2023-01-16 12:53:44 +0100

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -36,7 +36,7 @@ def main():
     st.markdown(html_text2, unsafe_allow_html=True)
     
     #adding header image
-    col1, col2, col3 = st.beta_columns([1,6,1])
+    col1, col2, col3 = st.columns([1,6,1])
     with col1:
      st.write("")
     
